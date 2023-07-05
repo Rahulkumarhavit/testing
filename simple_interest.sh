339b15327d08f9d0ce1bef3c94e93a0aa7d63e75
Rahul Kumar <92990906+Rahulkumarhavit@users.noreply.github.com>
2023-07-06 00:41:30 +0530

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,14 @@
+def calculate_interest(principal, rate, time):
+    interest = principal * rate * time
+    return interest
+
+# Input values
+principal_amount = 1000
+interest_rate = 0.05
+time_period = 3
+
+# Calculate interest
+interest_amount = calculate_interest(principal_amount, interest_rate, time_period)
+
+# Print the result
+print
